e591277dc4d1c7927dc3c0b93349b04c73711c5d
Marc Martin <martin.marc0227@gmail.com>
2024-03-23 12:09:56 +0800

    Finalized api calls

--- Rainfall.API/Controllers/RainfallController.cs
+++ Rainfall.API/Controllers/RainfallController.cs
@@ -1,21 +1,29 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Rainfall.API.Common.CustomExceptions;
 using Rainfall.API.Common.Models;
+using Rainfall.API.Common.Services.Abstracts;
 using System.Net;
 
 namespace Rainfall.API.Controllers
 {
     /// <summary>
     /// </summary>
+    [AllowAnonymous]
     [Route("[controller]")]
     [ApiController]
     public class RainfallController : ControllerBase
     {
         /// <summary>
         /// </summary>
-        public RainfallController()
+        public IRainfallService _rainfallService;
+        /// <summary>
+        /// </summary>
+        public RainfallController(
+             IRainfallService rainfallService)
         {
-
+            _rainfallService = rainfallService;
         }
         /// <summary>
         /// Operations relating to rainfall
@@ -29,19 +37,42 @@ namespace Rainfall.API.Controllers
         [ProducesResponseType(typeof(ErrorResponse), 400)]
         [ProducesResponseType(typeof(ErrorResponse), 404)]
         [ProducesResponseType(typeof(ErrorResponse), 500)]
-        public ActionResult<RainfallReadingResponse> GetRainfall([FromRoute] string stationId, [FromQuery] int count = 10)
+        public async Task<ActionResult<RainfallReadingResponse>> GetRainfall([FromRoute] string stationId, [FromQuery] int? count = 10)
         {
             try
             {
+                var item = await _rainfallService.GetRainfall(stationId, count);
+                return Ok(item);
+            }
+            catch (NullValueException ex)
+            {
+                ErrorResponse errors = new ErrorResponse();
+                errors.Message = ex.Message;
 
-                return Ok();
+                errors.Detail.Add(new ErrorDetail()
+                {
+                    Message = "stationId not found",
+                    PropertyName = "stationId",
+                });
+
+                return NotFound(errors);
+            }
+            catch (ArgumentException ex)
+            {
+                ErrorResponse errors = new ErrorResponse();
+                errors.Message = ex.Message;
+
+                errors.Detail.Add(new ErrorDetail()
+                {
+                    Message = "invalid count parameter passed",
+                    PropertyName = "count",
+                });
+
+                return BadRequest(errors);
             }
             catch (Exception ex)
             {
-                return BadRequest(new ErrorResponse
-                {
-                    Message = ex.Message,
-                });
+                throw ;
             }
         }
 
