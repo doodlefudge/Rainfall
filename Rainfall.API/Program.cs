6f59199a705e4e3db42db51d2f9bda6fbad98adc
Marc Martin <martin.marc0227@gmail.com>
2024-03-23 12:09:52 +0800

    cors issue update for localhost:3000

--- Rainfall.API/Program.cs
+++ Rainfall.API/Program.cs
@@ -5,6 +5,10 @@ using Rainfall.API.Common.Services.Abstracts;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(policyBuilder =>
+    policyBuilder.AddDefaultPolicy(policy =>
+        policy.WithOrigins("https://localhost:3000").AllowAnyHeader().AllowAnyMethod())
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -26,19 +30,23 @@ builder.Services.AddSwaggerGen(c =>
         });
     c.AddServer(new OpenApiServer()
     {
-        Url = "http://localhost:3000",
+        Url = "https://localhost:3000",
         Description = " Rainfall Api"
     });
     var filePath = Path.Combine(System.AppContext.BaseDirectory, "Rainfall.API.xml");
     c.IncludeXmlComments(filePath);
 });
 
+
 builder.Services.AddHttpClient<IRainfallApiHttpService, RainfallApiHttpService>(client =>
 {
     client.BaseAddress = new Uri(builder.Configuration["BaseUrl"]);
 });
 
+builder.Services.AddTransient<IRainfallService, RainfallService>();
+
 var app = builder.Build();
+app.UseCors();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
